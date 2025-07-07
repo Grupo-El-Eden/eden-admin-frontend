c888cd6bd53422157270685d73d682440b3571fd
Epixito <juandandres.moto@gmail.com>
2025-07-07 19:12:11 -0400

    feat(creacion de layouts 500,403,404,401 y creacion de path de layouts): layouts de errores
    Creacion de layouts de las paginas 500,404,403,401 a su vez se implementario los paths debidos para
    su funcionamiento

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import './index.css'
 import LoginForm from '@pages/Login/index'
 import { AppSidebar } from '@components/layout/app-sidebar'
+import Error500 from '@layouts/500'
+import Error401 from '@layouts/401'
+import Error403 from '@layouts/403'
+import Error404 from '@layouts/404'
 
 import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+
 
 function App() {
   return (
@@ -10,6 +15,10 @@ function App() {
       <Routes>
         <Route path='/*' element={<LoginForm/>} />
         <Route path='/dashboard' element={<div>Dashboard</div>} />
+        <Route path='/500' element={<Error500 />} />
+        <Route path='/401' element={<Error401 />} />
+        <Route path='/403' element={<Error403 />} />
+        <Route path='/404' element={<Error404 />} />
       </Routes>
     </Router>
   )
