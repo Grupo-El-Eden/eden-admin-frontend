ac8a83d3051f7c9da6bb38afe4b5746bce2180d7
Javier Diaz <70349374+javierd79@users.noreply.github.com>
2025-07-14 17:43:42 -0400

    Test: Test of all Errors has been passed

--- src/components/dev/DashboardErrorTest.tsx
+++ src/components/dev/DashboardErrorTest.tsx
@@ -1,24 +1,31 @@
-import { GET } from '@config/fetcher/Get'
+import { useEffect } from 'react'
 import { useErrorManager } from '@config/providers/ErrorManagerProvider'
 
 export function DashboardErrorTest() {
 	const { setError } = useErrorManager()
 
-  const { data, isLoading } = useQuery({
-    queryKey: ['dashboard'],
-    queryFn: () => GET('/api/dashboad'), // This request is to simulate an error
-    onError: (err) => {
-      setError(err as Error)
-    },
-  })
-
-  if (isLoading) {
-    return <div>Cargando dashboard…</div>
+  const errMessage = {
+    message: 'Error de prueba en el dashboard',
+    stack: 'Stack de error de prueba'
   }
+
+  const htmlError = `<div style='width: 100%; background-color: #fff; padding: 1rem; border-radius: 5px'><h1 style='color:red; font-weight: 700'>Error 404: Not found :-(</h1>
+    <p>I have no idea where that file is, sorry.  Are you sure you typed in the correct URL?</p>
+    <h1>Oops! Something went wrong...</h1>
+    <p>We seem to be having some technical difficulties. Hang tight.</p></div>`
+  
+  useEffect(() => {
+    setTimeout(() => {
+      setError(errMessage as Error)
+      setError(new Error(htmlError))
+      throw new Error('Error de prueba en el dashboard')
+    }, 1000)
+  }, [])
 
   return (
 		<div>
-			<pre>{JSON.stringify(data, null, 2)}</pre>
+			Some data will be displayed here.
+      <p>Check the error manager for the error.</p>
 		</div>
 	)
 }
