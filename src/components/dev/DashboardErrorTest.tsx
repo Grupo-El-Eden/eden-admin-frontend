95e598ae2da3be2a11675dc49ee9be50f2becd1c
Javier Diaz <70349374+javierd79@users.noreply.github.com>
2025-07-14 17:00:47 -0400

    Refactor: Change useEffect-Fetch cycle to useQuery of React Query
    Always use React Query, is a powerful tool to scalable frontend systems

--- src/components/dev/DashboardErrorTest.tsx
+++ src/components/dev/DashboardErrorTest.tsx
@@ -1,23 +1,24 @@
-import * as React from 'react'
+import { GET } from '@config/fetcher/Get'
 import { useErrorManager } from '@config/providers/ErrorManagerProvider'
 
 export function DashboardErrorTest() {
-  const { setError } = useErrorManager()
+	const { setError } = useErrorManager()
 
-  React.useEffect(() => {
-    fetch('/api/dashboard') // endpoint que devuelve error JSON
-      .then(async (res) => {
-        if (!res.ok) {
-          const body = await res.json()
-          // transformamos el cuerpo en Error para que lo capture el modal
-          throw new Error(JSON.stringify(body, null, 2))
-        }
-        return res.json()
-      })
-      .catch(err => {
-        setError(err as Error)
-      })
-  }, [setError])
+  const { data, isLoading } = useQuery({
+    queryKey: ['dashboard'],
+    queryFn: () => GET('/api/dashboad'), // This request is to simulate an error
+    onError: (err) => {
+      setError(err as Error)
+    },
+  })
 
-  return <div>Cargando dashboard…</div>
+  if (isLoading) {
+    return <div>Cargando dashboard…</div>
+  }
+
+  return (
+		<div>
+			<pre>{JSON.stringify(data, null, 2)}</pre>
+		</div>
+	)
 }
